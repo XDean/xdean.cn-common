835410803d2c987074b428688eb540ab23789962
dxu <dean.xu@asml.com>
2021-10-15 16:44:53 +0800

    fix handler not response bug

--- api/handler.ts
+++ api/handler.ts
@@ -1,7 +1,7 @@
-import {NextApiHandler, NextApiRequest, NextApiResponse} from "next/dist/shared/lib/utils";
-import {createHelper, Helper} from "./helper";
+import {NextApiHandler, NextApiRequest, NextApiResponse} from 'next/dist/shared/lib/utils';
+import {createHelper, Helper} from './helper';
 
-const Methods = ['GET', 'POST', 'DELETE', 'PATCH'] as const
+const Methods = ['GET', 'POST', 'DELETE', 'PATCH'] as const;
 type Method = typeof Methods[number] | 'DEFAULT'
 
 type ApiError = {
@@ -16,9 +16,9 @@ export function apiError(code: number, message: string, body?: any) {
     code,
     body: {
       message,
-      ...body
-    }
-  } as ApiError
+      ...body,
+    },
+  } as ApiError;
 }
 
 type Handler<T = any> = (params: { req: NextApiRequest, res: NextApiResponse<T>, helper: Helper }) => void | T | Promise<T>
@@ -30,38 +30,42 @@ type Options<T> = {
 }
 
 export function apiHandler<T>(options: Options<T>): NextApiHandler {
-  const {handler} = options
+  const {handler} = options;
   return async (req, res) => {
     const helper = createHelper(req, res);
     const callHandler = async (h: Handler) => {
-      return await Promise.resolve(h({req, res, helper}))
-    }
+      return await Promise.resolve(h({req, res, helper}));
+    };
     try {
       for (const m of Methods) {
         if (m === req.method && m in handler) {
-          const ret = await callHandler(handler[m]!)
-          if (!!ret) {
-            return res.status(200).json(ret)
+          const ret = await callHandler(handler[m]!);
+          if (!!ret || ret === null) {
+            return res.status(200).json(ret);
+          } else {
+            return;
           }
         }
       }
       if ('DEFAULT' in handler) {
-        const ret = await callHandler(handler['DEFAULT']!)
-        if (!!ret) {
-          return res.status(200).json(ret)
+        const ret = await callHandler(handler['DEFAULT']!);
+        if (!!ret || ret === null) {
+          return res.status(200).json(ret);
+        } else {
+          return;
         }
       }
       return res.status(405).json({
         code: 405,
-        support: Object.keys(handler)
-      })
+        support: Object.keys(handler),
+      });
     } catch (e: any) {
       if ('type' in e && e.type === 'ApiError') {
-        const ae = e as ApiError
-        return res.status(ae.code).json(ae.body)
+        const ae = e as ApiError;
+        return res.status(ae.code).json(ae.body);
       } else {
-        throw e
+        throw e;
       }
     }
-  }
+  };
 }
