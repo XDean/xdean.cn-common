fc22d3daca8f2802130f864dfc1430f8e4a63910
dxu <dean.xu@asml.com>
2021-12-31 17:23:15 +0800

    split get read and add read

--- api/impl/read.ts
+++ api/impl/read.ts
@@ -1,21 +1,35 @@
-import {database} from '../database';
 import {OkPacket} from 'mysql';
+import {database} from '../database';
 import {Read} from './domain';
+
 
 export async function getRead(params: {
   objId: string
   userId: number
 }): Promise<Read> {
+  await addRead(params);
+  return await getReadOnly(params);
+}
+
+export async function getReadOnly(params: {
+  objId: string
+}): Promise<Read> {
+  const {objId} = params;
+  const selectSql = `SELECT COUNT(*) AS unique_total, SUM(count) AS total
+      FROM xdean.obj_reads 
+      WHERE obj_id = ?`;
+  const result = await database.query<Read[]>(selectSql, [objId]);
+  return result[0];
+}
+
+export async function addRead(params: {
+  objId: string
+  userId: number
+}) {
   const {userId, objId} = params;
   const insertSql = `INSERT INTO xdean.obj_reads(obj_id, user_id)
                      VALUES (?, ?)
                      ON DUPLICATE KEY UPDATE count = IF(TIMESTAMPDIFF(SECOND, last_modified, CURRENT_TIMESTAMP) > 1800,
                                                         count + 1, count)`;
   await database.query<OkPacket>(insertSql, [objId, userId]);
-
-  const selectSql = `SELECT COUNT(*) AS unique_total, SUM(count) AS total
-      FROM xdean.obj_reads 
-      WHERE obj_id = ?`;
-  const result = await database.query<Read[]>(selectSql, [objId]);
-  return result[0];
 }
