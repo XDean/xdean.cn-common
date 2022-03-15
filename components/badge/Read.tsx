cc5df9cc4aae3d6c7a4a7cfe2a8bf82b71b08e9b
dxu <dean.xu@asml.com>
2022-03-15 14:04:05 +0800

    update tool layout

--- components/badge/Read.tsx
+++ components/badge/Read.tsx
@@ -1,17 +1,31 @@
-import React, {ReactNode} from 'react';
-import {Badge} from './Badge';
+import React, { ReactNode } from 'react';
+import useSWR from 'swr';
+import { Read as ReadState } from '../../api/impl/domain';
+import { fetchJsonApi } from '../../util/fetch';
+import { Badge } from './Badge';
 
 type Props = {
+  name?: string
   total?: number | ReactNode
   loading?: boolean
 }
 
 export const Read = (props: Props) => {
-  const {total, loading} = props;
+  const {name = '‍️阅读', total, loading} = props;
   return (
-    <Badge left={'👋 ‍️阅读'}
+    <Badge left={`👋 ‍️${name}`}
            right={total}
            loading={loading}
-           tooltip={'阅读量'}/>
+           tooltip={'${name}量'}/>
+  );
+};
+
+export const ReadWithAPI = ({id, ...rest}: { id: string } & Props) => {
+  const read = useSWR<ReadState>(`/api/read?id=${id}`, fetchJsonApi);
+  return (
+    <Read total={<div title={`${read.data?.unique_total}`}>{read.data?.total}</div> || 0}
+          loading={read.data === undefined}
+          {...rest}
+    />
   );
 };
