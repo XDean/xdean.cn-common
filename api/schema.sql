7cda963315f00d771fc80432d2cf1a9296e6e07a
Dean Xu <373216024@qq.com>
2022-03-26 10:51:09 +0800

    format sql

--- api/schema.sql
+++ api/schema.sql
@@ -1,40 +1,23 @@
-CREATE
-DATABASE IF NOT EXISTS xdean;
+CREATE DATABASE IF NOT EXISTS xdean;
 
-USE
-xdean;
+USE xdean;
 
 CREATE TABLE IF NOT EXISTS obj_likes
 (
-    obj_id VARCHAR
-(
-    256
-) NOT NULL,
-    user_id INT NOT NULL,
-    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    CONSTRAINT UNIQUE INDEX obj_likes_unique_idx
-(
-    obj_id,
-    user_id
-)
-    );
+    obj_id  VARCHAR(256)                        NOT NULL,
+    user_id INT                                 NOT NULL,
+    time    TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    CONSTRAINT UNIQUE INDEX obj_likes_unique_idx (obj_id, user_id)
+);
 
 CREATE TABLE IF NOT EXISTS obj_reads
 (
-    obj_id VARCHAR
-(
-    256
-) NOT NULL,
-    user_id INT NOT NULL,
-    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    CONSTRAINT UNIQUE INDEX obj_reads_unique_idx
-(
-    obj_id,
-    user_id
-)
-    );
+    obj_id  VARCHAR(256)                        NOT NULL,
+    user_id INT                                 NOT NULL,
+    time    TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    CONSTRAINT UNIQUE INDEX obj_reads_unique_idx (obj_id, user_id)
+);
 
 ALTER TABLE obj_reads
-    ADD `count` INT DEFAULT 1 NOT NULL,
-    ADD `last_modified` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL ON
-UPDATE CURRENT_TIMESTAMP;
+    ADD `count`         INT       DEFAULT 1                 NOT NULL,
+    ADD `last_modified` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP;
