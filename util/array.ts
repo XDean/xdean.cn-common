a47fd014809f4815505329e042e98d27c5e20b44
Dean Xu <373216024@qq.com>
2022-03-26 14:52:39 +0800

    [guobiao] Fan table

--- util/array.ts
+++ util/array.ts
@@ -35,3 +35,17 @@ export function contentEquals<T>(
   }
   return true;
 }
+
+export function groupBy<T, K extends keyof any>(list: T[], keyGetter: (t: T) => K) {
+  const map = new Map<K, T[]>();
+  list.forEach((item) => {
+    const key = keyGetter(item);
+    const collection = map.get(key);
+    if (!collection) {
+      map.set(key, [item]);
+    } else {
+      collection.push(item);
+    }
+  });
+  return map;
+}
